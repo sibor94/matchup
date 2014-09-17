bd69a8e54dba5ee42405920cedda91daebf9d2b5
Boris Stievet <boris.stievet@gmail.com>
2014-09-17 21:08:04 +0200

    Fini avec les pages statiques

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ end
 group :test do
   gem 'rspec', '3.1.0'
   gem 'webrat', '0.7.1'
+  gem 'spork', '0.9.0.rc5'
 end
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
   def home
+     @titre = "Accueil"
   end
-
+  def about
+     @titre = "A propos"
+  end
   def contact
+    @titre = "Contact"
   end
 end
